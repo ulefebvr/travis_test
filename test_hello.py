07fc3289a22d25cd2cdb0049598554d24e04504a
Umi LEFEBVRE <ulefebvr@e1r1p8.42.fr>
2016-01-19 15:25:33 +0100

    yolo

--- test_hello.py
+++ test_hello.py
@@ -6,6 +6,10 @@ class CheckHello(unittest.TestCase):
         p1 = subprocess.check_output(["./hello"])
         # print p1
         self.assertEqual(p1, "Helloworld !\n")
+    def test_output2(self):
+        p1 = subprocess.check_output(["./hello"])
+        # print p1
+        self.assertEqual(p1, "Helloworld !\n")
 
 if __name__ == '__main__':
     unittest.main()
