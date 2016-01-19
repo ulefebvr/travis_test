0ba966edc925422dd96d2e5e774f5e402e133345
Umi LEFEBVRE <ulefebvr@e1r1p8.42.fr>
2016-01-19 15:24:04 +0100

    first

---
+++ helloworld.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+    printf("%s\n", "Helloworld !");
+    return 0;
+}
---
+++ test_hello.py
@@ -0,0 +1,11 @@
+import unittest
+import subprocess
+
+class CheckHello(unittest.TestCase):
+    def test_output(self):
+        p1 = subprocess.check_output(["./hello"])
+        # print p1
+        self.assertEqual(p1, "Helloworld !\n")
+
+if __name__ == '__main__':
+    unittest.main()
